c836f9c15d4a5f189e69028a74786becc9009e6f
Andrew Stein <ABStein@github.com>
2019-09-18 09:30:04 -0500

    added more tests and deleted unused file

--- test/lib/review_scraper_test.rb
+++ test/lib/review_scraper_test.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'httparty'
 
 RSpec.describe ReviewScraper do
-	describe '#reviews' do
+	describe '#review_scraper' do
 		let(:bad_scraper) { ReviewScraper.new('https://www.lendingng/44068646') }
 		let(:good_scraper) { ReviewScraper.new('https://www.lendingtree.com/reviews/mortgage/triumph-lending/44068646') } 
 
@@ -13,6 +13,24 @@ RSpec.describe ReviewScraper do
 
 		it 'should parse the page' do
 			expect(good_scraper).to be_a(ReviewScraper)
+		end
+
+		it 'should return the lender name' do
+			expect(good_scraper).to be_a(ReviewScraper)
+			lender = good_scraper.lender_name
+			expect(lender).to eq('Triumph Lending')
+		end
+
+		it 'should return a review' do
+			expect(good_scraper).to be_a(ReviewScraper)
+			get_reviews = good_scraper.reviews[0]
+			expect(get_reviews).to include({
+				review_author: "Great experience ",
+				review_body: "These folks are really kind and helpful in the whole process which they make simple. I would recommend anyone to them that need financial support ",
+				review_author: "April from GROVERTOWN, IN ",
+				review_date: "Reviewed in September 2019",
+				star_count: "(5 of 5)"
+			})
 		end
 	end
 end
